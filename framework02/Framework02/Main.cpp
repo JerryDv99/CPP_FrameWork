f97d259263489a2015410b2e5de5fdc8a75e6a8f
devmanjerry@gmail.com <devmanjerry@gmail.com>
2022-09-07 14:19:08 +0900

    d

---
+++ framework02/Framework02/Main.cpp
@@ -0,0 +1,27 @@
+#include "MainUpdate.h"
+
+
+
+int main(void)
+{
+	system("title 홍길동 : 게임이름");
+	system("mode con cols = 160 lines = 50");
+
+	MainUpdate Main;
+	Main.Start();
+
+	ULONGLONG Time = GetTickCount64();
+	
+	while (true)
+	{
+		if (Time <= GetTickCount64())
+		{
+			system("cls");
+
+			Main.Update();
+			Main.Render();
+		}
+	}
+
+	return 0;
+}
